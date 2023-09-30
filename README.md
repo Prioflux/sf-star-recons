a1998124d7dd4f0350de9844927486012cc1ec29
Wouter Bruynsteen <wouter@prioflux.com>
2023-09-30 23:38:55 +0200

    Add initial script

---
+++ README.md
@@ -0,0 +1,58 @@
+# Overview
+
+This repository contains an example of a node.js script that will star a specific reconciliation in all non-locked period of all companies inside Silverfin
+
+Silverfin API documentation: https://developer.silverfin.com/reference/get-started-1
+
+# Running the script
+
+## Prerequisites
+
+1. Node.js (https://nodejs.org/en/download/)
+
+   - The script is written in JavaScript and node.js is needed to run the script locally.
+
+2. Valid Silverfin environment (in other words, you already have a firm in Silverfin and your user is added)
+
+   This script assumes that the following things are already set-up in your Silverfin environment:
+
+   - The list of companies for which you want to run the script are already added in Silverfin and the companies + periods in Silverfin have the reconciliation you want to star already added.
+
+3. Silverfin API credentials
+
+   The Silverfin API uses OAuth2, in order to use the API you need to have contacted Silverfin Support to create an API client in Silverfin and obtain the credentials (client ID / secret) to generate the access token.
+
+## Setup
+
+1. Add a local `.env` file with the following content:
+
+```
+# This is a valid access token generated through for the Silverfin API (can be generated through another app like Postman and pasted here)
+SILVERFIN_TOKEN=<your token>
+
+# This is the firm ID of the firm you want to push the data to
+SILVERFIN_FIRM_ID=<your firm id>
+
+# The handle of the reconciliation you want to star
+RECONCILIATION_HANDLE=<handle of the reconciliation you want to star>
+```
+
+2. Install the dependencies
+
+The `axios` package is used to make the API calls to Silverfin.
+
+The `dotenv` package is used to read the `.env` file.
+
+Install the dependencies with the following command:
+
+```
+npm install
+```
+
+## Run the script
+
+The script can be run with the following command:
+
+```
+npm start
+```
---
+++ index.js
@@ -0,0 +1,110 @@
+const axios = require("axios");
+require("dotenv").config();
+const FormData = require("form-data");
+
+// =======================================
+// START OF DATA TO BE ADDED BY THE USER
+// =======================================
+
+// The SILVERFIN_TOKEN is a valid access token for the Silverfin API that's created through another application like Postman before running the script
+// The SILVERFIN_FIRM_ID is the id of the firm that can be found in the URL of the Silverfin web application (e.g. https://live.getsilverfin.com/f/13827/)
+const { SILVERFIN_TOKEN, SILVERFIN_FIRM_ID, RECONCILIATION_HANDLE } =
+  process.env;
+
+// =======================================
+// END OF DATA TO BE ADDED BY THE USER
+// =======================================
+
+// Create an axios instance with the base URL and the authorization header that should be used for each Silverfin API request
+const defaultHeaders = {
+  Authorization: `Bearer ${SILVERFIN_TOKEN}`,
+  Accept: "application/json",
+};
+const axiosInstance = axios.create({
+  baseURL: "https://live.getsilverfin.com/api/v4",
+  Accept: "application/json",
+  headers: {
+    ...defaultHeaders,
+  },
+});
+
+const starReconciliation = async () => {
+  try {
+    // Loop through each company
+    let page = 1;
+    let perPage = 10;
+    let companies = [];
+
+    do {
+      companies = await axiosInstance.get(`/f/${SILVERFIN_FIRM_ID}/companies`, {
+        params: {
+          page,
+          per_page: perPage,
+        },
+      });
+
+      for (let company of companies.data) {
+        // Run through each period of the company
+        const periods = await axiosInstance.get(
+          `/f/${SILVERFIN_FIRM_ID}}/companies/${company.id}/periods`
+        );
+
+        for (let period of periods.data) {
+          let reconsPage = 1;
+          let reconsPerPage = 30;
+          let reconciliations = [];
+
+          // Search through the reconciliations for each period to find the reconciliation with a specific handle
+          do {
+            reconciliations = await axiosInstance.get(
+              `/f/${SILVERFIN_FIRM_ID}/companies/${company.id}/periods/${period.id}/reconciliations`,
+              {
+                params: {
+                  page: reconsPage,
+                  per_page: reconsPerPage,
+                },
+              }
+            );
+
+            const starredRecon = reconciliations.data.find(
+              (recon) => recon.handle === RECONCILIATION_HANDLE
+            );
+
+            // Star the reconciliation in non-locked ledgers
+            if (starredRecon) {
+              let data = new FormData();
+              data.append("starred", "true");
+
+              const response = await axiosInstance.post(
+                `/f/${SILVERFIN_FIRM_ID}/companies/${company.id}/periods/${period.id}/reconciliations/${starredRecon.id}`,
+                data,
+                {
+                  headers: {
+                    "Content-Type": "multipart/form-data",
+                    ...defaultHeaders,
+                  },
+                }
+              );
+
+              console.log(
+                `Starred status of reconciliation '${RECONCILIATION_HANDLE}' in company ${company.name} and period ${period.id} set to ${response.data.starred}`
+              );
+              console.log("");
+              break;
+            }
+
+            reconsPage++;
+          } while (reconciliations.data.length === reconsPerPage);
+        }
+      }
+
+      page++;
+    } while (companies.data.length === perPage);
+  } catch (error) {
+    console.log("====================================");
+    console.log(error);
+    console.log("An error occured");
+  }
+};
+
+starReconciliation();
